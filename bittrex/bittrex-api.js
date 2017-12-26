1245cf415714ba5b11deb021db6ef1c163a0c004
Miguel Salinas Gancedo <miguel@thingtrack.com>
2017-12-26 12:00:00 +0100

    last update

---
+++ bittrex/bittrex-api.js
@@ -0,0 +1,199 @@
+module.exports = function(RED) {
+    "use strict";
+
+    // load bittrex package
+    var bittrex = require('node-bittrex-api');
+
+    // load RED settings
+    var settings = RED.settings;
+
+    // Bittrex: API
+    function BittrexApi(config) {
+        RED.nodes.createNode(this, config);
+
+        var node = this;
+
+        // execute Bittrex API
+        node.on('input', function(msg) {
+            bittrex.options({
+                apikey: config.apikey,
+                apisecret: config.apisecret
+            });
+
+            if (msg.payload.api == "getticker") {
+                bittrex.getticker( msg.payload.options, function( data, err ) {
+                    if (err) {
+                        node.status({fill:"red", shape: "ring", text: "Get ticker error"});
+                        node.error("Get ticker error", error);
+
+                        msg.error = err;
+                        node.send(msg);
+                    }
+
+                    msg.payload = data;
+
+                    node.send(msg);
+                });
+            }
+            else if (msg.payload.api == "getbalances") {
+                bittrex.getbalances( function( data, err ) {
+                    if (err) {
+                        node.status({fill:"red", shape: "ring", text: "Get balances"});
+                        node.error("Get balances", error);
+
+                        msg.error = err;
+                        node.send(msg);
+                    }
+
+                    msg.payload = data;
+
+                    node.send(msg);
+                });
+            }
+            else if (msg.payload.api == "getmarkethistory") {
+                bittrex.getmarkethistory( msg.payload.options, function( data, err ) {
+                    if (err) {
+                        node.status({fill:"red", shape: "ring", text: "Get market history error"});
+                        node.error("Get market history error", error);
+
+                        msg.error = err;
+                        node.send(msg);
+                    }
+
+                    msg.payload = data;
+
+                    node.send(msg);
+                });
+            }
+            else if (msg.payload.api == "getmarketsummaries") {
+                bittrex.getmarketsummaries( function( data, err ) {
+                    if (err) {
+                        node.status({fill:"red", shape: "ring", text: "Get market summaries error"});
+                        node.error("Get market summaries error", error);
+
+                        msg.error = err;
+                        node.send(msg);
+                    }
+
+                    msg.payload = data;
+
+                    node.send(msg);
+                });
+            }
+            else if (msg.payload.api == "getmarketsummary") {
+                bittrex.getmarketsummary( msg.payload.options, function( data, err ) {
+                    if (err) {
+                        node.status({fill:"red", shape: "ring", text: "Get market summary error"});
+                        node.error("Get market summary error", error);
+
+                        msg.error = err;
+                        node.send(msg);
+                    }
+
+                    msg.payload = data;
+
+                    node.send(msg);
+                });
+            }
+            else if (msg.payload.api == "getorderbook") {
+                bittrex.getorderbook( msg.payload.options, function( data, err ) {
+                    if (err) {
+                        node.status({fill:"red", shape: "ring", text: "Get orderbook error"});
+                        node.error("Get orderbook error", error);
+
+                        msg.error = err;
+                        node.send(msg);
+                    }
+
+                    msg.payload = data;
+
+                    node.send(msg);
+                });
+            }
+            else if (msg.payload.api == "getwithdrawalhistory") {
+                bittrex.getwithdrawalhistory( msg.payload.options, function( data, err ) {
+                    if (err) {
+                        node.status({fill:"red", shape: "ring", text: "Get withdrawal history error"});
+                        node.error("Get withdrawal history error", error);
+
+                        msg.error = err;
+                        node.send(msg);
+                    }
+
+                    msg.payload = data;
+
+                    node.send(msg);
+                });
+            }
+            else if (msg.payload.api == "getdepositaddress") {
+                bittrex.getdepositaddress( msg.payload.options, function( data, err ) {
+                    if (err) {
+                        node.status({fill:"red", shape: "ring", text: "Get deposit address error"});
+                        node.error("Get deposit address error", error);
+
+                        msg.error = err;
+                        node.send(msg);
+                    }
+
+                    msg.payload = data;
+
+                    node.send(msg);
+                });
+            }
+            else if (msg.payload.api == "getdeposithistory") {
+                bittrex.getdeposithistory( msg.payload.options, function( data, err ) {
+                    if (err) {
+                        node.status({fill:"red", shape: "ring", text: "Get deposit history error"});
+                        node.error("Get deposit history error", error);
+
+                        msg.error = err;
+                        node.send(msg);
+                    }
+
+                    msg.payload = data;
+
+                    node.send(msg);
+                });
+            }
+            else if (msg.payload.api == "getbalance") {
+                bittrex.getbalance( msg.payload.options, function( data, err ) {
+                    if (err) {
+                        node.status({fill:"red", shape: "ring", text: "Get balance error"});
+                        node.error("Get balance error", error);
+
+                        msg.error = err;
+                        node.send(msg);
+                    }
+
+                    msg.payload = data;
+
+                    node.send(msg);
+                });
+            }
+            else if (msg.payload.api == "withdraw") {
+                bittrex.withdraw( msg.payload.options, function( data, err ) {
+                    if (err) {
+                        node.status({fill:"red", shape: "ring", text: "Withdraw error"});
+                        node.error("Withdraw error", error);
+
+                        msg.error = err;
+                        node.send(msg);
+                    }
+
+                    msg.payload = data;
+
+                    node.send(msg);
+                });
+            }
+            else {
+                node.status({fill:"red", shape: "ring", text: "Bittrex API not exist"});
+                node.error("Bittrex API not exist");
+
+                msg.error = "Bittrex API not exist";
+                node.send(msg);
+            }
+        });
+    }
+
+    RED.nodes.registerType('bittrex-api', BittrexApi);
+};
---
+++ bittrex/bittrex-websocket.js
@@ -0,0 +1,71 @@
+module.exports = function(RED) {
+    "use strict";
+
+    // load bittrex package
+    var bittrex = require('node-bittrex-api');
+
+    // load RED settings
+    var settings = RED.settings;
+
+    // Bittrex: WebSocket
+    function BittrexWebSocket(config) {
+        RED.nodes.createNode(this, config);
+
+        var node = this;
+        var websocketClient;
+
+        // configure bittrex connection
+        bittrex.options({
+            apikey: config.apikey,
+            apisecret: config.apisecret,
+            websockets: {
+                onConnect: function() {
+                    node.status({fill: "green", shape: "ring", text: "Connected"});
+                    console.info("Websocket connected at " + new Date());
+
+                    // configure bittrex service handlers
+                    websocketClient.serviceHandlers.connectFailed = function(error) {
+                        node.status({fill: "red", shape: "ring", text: "Connect failed"});
+                        node.error("Websocket connectFailed", error);
+                    };
+
+                    websocketClient.serviceHandlers.onerror = function(error) {
+                        node.status({fill: "red", shape: "ring", text: "Error"});
+                        node.error("Websocket error", error);
+                    };
+
+                    websocketClient.serviceHandlers.connectionLost = function(error) {
+                        node.status({fill:"red", shape: "ring", text: "Connection lost"});
+                        node.error("Connection Lost", error);
+                    };
+
+                    // subscribe to bittrex websocket
+                    bittrex.websockets.listen(function(data) {
+                        var msg = {};
+
+                        if (data.M === 'updateSummaryState') {
+                            data.A.forEach(function(data_for) {
+                                data.A.forEach(function(data_for) {
+                                    msg.payload = data_for.Deltas;
+
+                                    node.send(msg);
+                                });
+                            });
+                        }
+                    });
+                },
+                onDisconnect: function() {
+                    node.status({fill: "yellow", shape: "ring", text: "Disconnected"});
+                    node.warn("Websocket disconnected");
+                }
+            }
+        });
+
+        // connect to bittrex websocket
+        bittrex.websockets.client(function(client) {
+            websocketClient = client;
+        });
+    }
+
+    RED.nodes.registerType('bittrex-websocket', BittrexWebSocket);
+};
